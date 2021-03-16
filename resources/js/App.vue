ac4fb8053224784e53ea1f600e6d1eeb9a8a796f
Mubasshir Hussain <hussain@lhgraphics.com>
2021-03-16 18:54:22 +0500

    initial-commit

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>App Component</h1>
+        <p>This is the app component</p>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
